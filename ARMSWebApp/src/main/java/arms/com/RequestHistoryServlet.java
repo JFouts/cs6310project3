4544f75d9206b910c3a82fded1d6e8a7b6f88601
leannmestrada <leannmestrada@gmail.com>
2016-07-24 16:23:35 -0400

    Set up request history to work with the StudentRequest objects. Waiting on DB function to be fully operational

--- ARMSWebApp/src/main/java/arms/com/RequestHistoryServlet.java
+++ ARMSWebApp/src/main/java/arms/com/RequestHistoryServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import arms.db.ARMDatabase;
+import arms.db.Course;
 import arms.db.Student;
 import arms.db.StudentRequest;
 
@@ -33,6 +35,19 @@ public class RequestHistoryServlet extends ARMSServlet {
     	request.setAttribute("userId", userId);
     	
     	ARMDatabase api = ARMDatabase.getDatabase();
+    	
+    	Map<Integer, String> courses = new HashMap<Integer, String>();
+		try {
+			for (Course c: api.getCatalog())
+				courses.put(c.getId(), c.getName());
+		} catch (Exception e) {
+			request.setAttribute("error", e.toString());
+			e.printStackTrace();
+			request.getRequestDispatcher("WEB-INF/Error.jsp").forward(request, response);
+			return;
+		}
+		request.setAttribute("courses", courses);
+    	
     	Student student = null;
     	try {
 			student = api.getStudent(userId);
@@ -43,20 +58,19 @@ public class RequestHistoryServlet extends ARMSServlet {
 			return;
 		}
     	
-    	//TODO: Some db call here
-    	ArrayList<StudentRequest> requests = new ArrayList<StudentRequest>();
+    	//TODO: Some db call here using student
+    	ArrayList<StudentRequest> requestList = new ArrayList<StudentRequest>();
+    	StudentRequest sr = new StudentRequest(1, new java.sql.Timestamp(9999));
+    	Map<Integer,Integer> srs = new HashMap<Integer,Integer>();
+    	srs.put(1, 1);
+    	srs.put(2, 1);
+    	srs.put(3, -1);
+    	sr.setSchedule(srs);
+    	requestList.add(sr);
     	
-//    	TempStudentSchedule[] schedule = new TempStudentSchedule[] {
-//    			new TempStudentSchedule((String) courses.get(1), 1),
-//    			new TempStudentSchedule((String) courses.get(2), 2),
-//    	};
-//    	TempStudentRequest[] requestList = new TempStudentRequest[] {
-//    			new TempStudentRequest(3, new Date(), schedule),
-//    			new TempStudentRequest(2, new Date(), schedule),
-//    			new TempStudentRequest(1, new Date(), schedule),
-//    	};
+    	request.setAttribute("requestList", requestList);
     	
-    	//request.setAttribute("requestList", requestList);
+    	
     	
 		request.getRequestDispatcher("WEB-INF/RequestHistory.jsp").forward(request, response);
     }
