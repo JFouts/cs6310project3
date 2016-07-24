cabe6cde6fd093db1a0537629dd8bb32e167a866
software-engineering <ubuntu@engineering.(none)>
2016-07-24 02:56:41 -0400

    Updated the Summary report page. The formatting needs some love though.

--- ARMSWebApp/src/main/java/arms/com/ARMSServlet.java
+++ ARMSWebApp/src/main/java/arms/com/ARMSServlet.java
@@ -7,8 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import arms.db.ARMDatabase;
+import arms.db.Administrator;
+import arms.db.Student;
+
 public class ARMSServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    protected int userId = -1;
+    protected boolean isAdmin = false;
+    protected boolean isStudent = false;
     
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
@@ -16,15 +24,26 @@ public class ARMSServlet extends HttpServlet {
     	String userIdString = request.getParameter("userId");
     	if(userIdString != null && !"".equals(userIdString))
     	{
-    		request.setAttribute("isLoggedIn", true);
     		int userId = Integer.parseInt(userIdString);
-    		request.setAttribute("userId", userId);
-    		
+    		ARMDatabase api = ARMDatabase.getDatabase();
     		boolean isAdmin = false;
-    		if(userId == 2) {
-    			isAdmin = true;
-    		}
+    		boolean isStudent = false;
     		
+    		try {
+				Student student = api.getStudent(userId);
+				if(student != null && student.getStudentId() >= 0) {
+					isStudent = true;
+				} else {
+					isAdmin = true;
+				}
+			} catch (Exception e) {
+				request.setAttribute("error", e.toString());
+				e.printStackTrace();
+				request.getRequestDispatcher("WEB-INF/Error.jsp").forward(request, response);
+			}
+    		
+    		request.setAttribute("isLoggedIn", true);
+    		request.setAttribute("userId", userId);
     		request.setAttribute("isAdmin", isAdmin);
     	}    	
     }
