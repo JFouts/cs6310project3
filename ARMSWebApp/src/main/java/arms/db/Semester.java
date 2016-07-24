daa58e3a8964ada79dde702b2aaa4495fc8955c1
software-engineering <ubuntu@engineering.(none)>
2016-07-24 11:38:07 -0400

    Missing file

---
+++ ARMSWebApp/src/main/java/arms/db/Semester.java
@@ -0,0 +1,43 @@
+package arms.db;
+
+public class Semester {
+	
+	private int id;
+	private String term;
+	private int year;
+	
+	public Semester(int id) {
+		String[] terms = {"Spring", "Summer", "Fall"};
+		int startingYear = 2016;
+		int startingTermId = 2;
+		int termId = id % 3;
+		this.year = (id+startingTermId / 3) + startingYear;
+		this.id = id;
+		this.term = terms[termId];
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTerm() {
+		return term;
+	}
+
+	public void setTerm(String term) {
+		this.term = term;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
